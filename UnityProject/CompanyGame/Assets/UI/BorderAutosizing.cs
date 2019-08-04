b1d7e5f35ee743d365b0a9cf4967db248e3518ca
cristisevastian <cristisevastian@hotmail.com>
2019-08-04 19:01:05 +0300

    UI Autosize script added (still needs work to update it's children)

---
+++ UnityProject/CompanyGame/Assets/UI/BorderAutosizing.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderAutosizing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AdjustSize()
+    {
+        Vector3 ThisSize = this.transform.GetComponent<RectTransform>().sizeDelta;
+
+        Vector3 BottomSize = this.transform.Find("UIBorder_Bottom").GetComponent<RectTransform>().sizeDelta;
+        Vector3 TopSize = this.transform.Find("UIBorder_Top").GetComponent<RectTransform>().sizeDelta;
+        Vector3 LeftSize = this.transform.Find("UIBorder_Left").GetComponent<RectTransform>().sizeDelta;
+        Vector3 RightSize = this.transform.Find("UIBorder_Right").GetComponent<RectTransform>().sizeDelta;
+        Vector3 BottomLeftCornerDownSize = this.transform.Find("UIBorder_BottomLeftCornerDown").GetComponent<RectTransform>().sizeDelta;
+        Vector3 BottomLeftCornerUpSize = this.transform.Find("UIBorder_BottomLeftCornerUp").GetComponent<RectTransform>().sizeDelta;
+        Vector3 TopLeftCornerDownSize = this.transform.Find("UIBorder_TopLeftCornerDown").GetComponent<RectTransform>().sizeDelta;
+        Vector3 TopLeftCornerUpSize = this.transform.Find("UIBorder_TopLeftCornerUp").GetComponent<RectTransform>().sizeDelta;
+        Vector3 TopRightCornerDownSize = this.transform.Find("UIBorder_TopRightCornerDown").GetComponent<RectTransform>().sizeDelta;
+        Vector3 TopRightCornerUpSize = this.transform.Find("UIBorder_TopRightCornerUp").GetComponent<RectTransform>().sizeDelta;
+        Vector3 BottomRightCornerDownSize = this.transform.Find("UIBorder_BottomRightCornerDown").GetComponent<RectTransform>().sizeDelta;
+        Vector3 BottomRightCornerUpSize = this.transform.Find("UIBorder_BottomRightCornerUp").GetComponent<RectTransform>().sizeDelta;
+
+        BottomSize.x = ThisSize.x - 2 * BottomSize.y;
+        TopSize = BottomSize;
+        LeftSize.x = BottomSize.y;
+        LeftSize.y = ThisSize.y - 2*BottomSize.y;
+        RightSize.x = BottomSize.y;
+        RightSize.y = LeftSize.y;
+        BottomLeftCornerDownSize.x = BottomSize.y;
+        BottomLeftCornerDownSize.y = BottomSize.y;
+        BottomLeftCornerUpSize.x = BottomSize.y; 
+        BottomLeftCornerUpSize.y = BottomSize.y; 
+        TopLeftCornerDownSize.x = BottomSize.y; 
+        TopLeftCornerDownSize.y = BottomSize.y; 
+        TopLeftCornerUpSize.x = BottomSize.y; 
+        TopLeftCornerUpSize.y = BottomSize.y; 
+        TopRightCornerDownSize.x = BottomSize.y; 
+        TopRightCornerDownSize.y = BottomSize.y; 
+        TopRightCornerUpSize.x = BottomSize.y; 
+        TopRightCornerUpSize.y = BottomSize.y; 
+        BottomRightCornerDownSize.x = BottomSize.y;
+        BottomRightCornerDownSize.y = BottomSize.y;
+        BottomRightCornerUpSize.x = BottomSize.y; 
+        BottomRightCornerUpSize.y = BottomSize.y;
+
+        this.transform.Find("UIBorder_Bottom").GetComponent<RectTransform>().sizeDelta = BottomSize;
+        this.transform.Find("UIBorder_Top").GetComponent<RectTransform>().sizeDelta = TopSize;
+        this.transform.Find("UIBorder_Left").GetComponent<RectTransform>().sizeDelta = LeftSize;
+        this.transform.Find("UIBorder_Right").GetComponent<RectTransform>().sizeDelta = RightSize;
+        this.transform.Find("UIBorder_BottomLeftCornerDown").GetComponent<RectTransform>().sizeDelta = BottomLeftCornerDownSize;
+        this.transform.Find("UIBorder_BottomLeftCornerUp").GetComponent<RectTransform>().sizeDelta = BottomLeftCornerUpSize;
+        this.transform.Find("UIBorder_TopLeftCornerDown").GetComponent<RectTransform>().sizeDelta = TopLeftCornerDownSize;
+        this.transform.Find("UIBorder_TopLeftCornerUp").GetComponent<RectTransform>().sizeDelta = TopLeftCornerUpSize;
+        this.transform.Find("UIBorder_TopRightCornerDown").GetComponent<RectTransform>().sizeDelta = TopRightCornerDownSize;
+        this.transform.Find("UIBorder_TopRightCornerUp").GetComponent<RectTransform>().sizeDelta = TopRightCornerUpSize;
+        this.transform.Find("UIBorder_BottomRightCornerDown").GetComponent<RectTransform>().sizeDelta = BottomRightCornerDownSize;
+        this.transform.Find("UIBorder_BottomRightCornerUp").GetComponent<RectTransform>().sizeDelta = BottomRightCornerUpSize;
+    }
+}
---
+++ UnityProject/CompanyGame/Assets/UI/UIAutosizing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAutosizing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AdjustSize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AdjustSize()
+    {
+        Vector3 ThisSize = this.transform.GetComponent<RectTransform>().sizeDelta;
+
+        Vector3 BottomSize = this.transform.Find("UIBottom").GetComponent<RectTransform>().sizeDelta;
+        Vector3 LeftSize = this.transform.Find("UILeft").GetComponent<RectTransform>().sizeDelta;
+        Vector3 BottomLeftCornerSize = this.transform.Find("UIBottomLeftCorner").GetComponent<RectTransform>().sizeDelta;
+        Vector3 Border = this.transform.Find("UIBorder").GetComponent<RectTransform>().sizeDelta;
+
+        BottomSize.x = ThisSize.x - BottomLeftCornerSize.x;
+        BottomSize.y = BottomLeftCornerSize.y;
+        LeftSize.x = BottomLeftCornerSize.x;
+        LeftSize.y = ThisSize.y - BottomLeftCornerSize.y;
+        Border.x = BottomSize.x;
+        Border.y = LeftSize.y;
+
+        this.transform.Find("UIBottom").GetComponent<RectTransform>().sizeDelta = BottomSize;
+        this.transform.Find("UILeft").GetComponent<RectTransform>().sizeDelta = LeftSize;
+        this.transform.Find("UIBottomLeftCorner").GetComponent<RectTransform>().sizeDelta = BottomLeftCornerSize;
+        this.transform.Find("UIBorder").GetComponent<RectTransform>().sizeDelta = Border;
+        this.transform.Find("UIBorder").GetComponent<BorderAutosizing>().AdjustSize();
+    }
+}
