47808366f397bf7234645ec8bd931a2ab142d907
cristisevastian <cristisevastian@hotmail.com>
2019-08-12 19:46:11 +0300

    Unity Porject created.

---
+++ UnityProject/CompanyGameR/Assets/UI/UIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public float UIScale = 1f;
+    private const float ReferenceSize = 128f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float UISize = UIScale * ReferenceSize;
+        Transform bottomBezel = this.transform.Find("BottomBezel");
+        bottomBezel.GetComponent<RectTransform>().sizeDelta = new Vector2(0f, UISize);
+        
+        bottomBezel.GetComponent<Image>().color = ColorPool.getBezelColorFromHex(ColorPool.HEX_PRIMARYBEZELS);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
